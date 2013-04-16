86976bb6e5362d0cdc7fbeab1d068abc55300a80
Kamil Dziemianowicz <dziemian_rec@wp.pl>
2013-04-16 14:20:30 +0200

    48157853 Removing user's identities when removing user from DB.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   has_one :api_key, dependent: :destroy
   has_many :time_log_entries, dependent: :destroy
-  has_many :identities
+  has_many :identities, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { on: :create }, length: { minimum: 8 }, if: :password_digest_changed?
