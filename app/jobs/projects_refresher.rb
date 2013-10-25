81bfd70a48551d812dc80d57d91a1734d2368cbd
Marcin Nieborak <marcin.nieborak@gmail.com>
2013-10-25 13:46:49 +0200

    Finish renaming identities to integrations

--- app/jobs/projects_refresher.rb
+++ app/jobs/projects_refresher.rb
@@ -1,21 +1,21 @@
 class ProjectsRefresher
 
   def run
-    Identity.find_each do |identity|
-      unless identity.last_projects_refresh_at.present? && identity.last_projects_refresh_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
-        case identity.type
-          when 'PivotalTrackerIdentity'
-            PivotalTrackerProjectsFetcher.new.fetch_projects identity
-            identity.projects do |project|
-              PivotalTrackerProjectsFetcher.new.fetch_tasks project, identity unless project.updated_at.present? && project.updated_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
+    Integration.find_each do |integration|
+      unless integration.last_projects_refresh_at.present? && integration.last_projects_refresh_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
+        case integration.type
+          when 'PivotalTrackerIntegration'
+            PivotalTrackerProjectsFetcher.new.fetch_projects integration
+            integration.projects do |project|
+              PivotalTrackerProjectsFetcher.new.fetch_tasks project, integration unless project.updated_at.present? && project.updated_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
             end
-          when 'GitHubIdentity'
-            GitHubProjectsFetcher.new.fetch_projects identity
-            identity.projects do |project|
-              GitHubProjectsFetcher.new.fetch_tasks_for_project project, identity unless project.updated_at.present? && project.updated_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
+          when 'GitHubIntegration'
+            GitHubProjectsFetcher.new.fetch_projects integration
+            integration.projects do |project|
+              GitHubProjectsFetcher.new.fetch_tasks_for_project project, integration unless project.updated_at.present? && project.updated_at > (DateTime.now - AppConfig.projects_refresh_period.hours)
             end 
         end
-        identity.update_attribute('last_projects_refresh_at', DateTime.now)
+        integration.update_attribute('last_projects_refresh_at', DateTime.now)
       end
     end
   end
